9fff8ac7dc189bc4a49a01088f12577b20b24179
Olav-4HA <112445@emmauscollege.nl>
2023-09-08 08:00:20 +0000

    B

--- script.js
+++ script.js
@@ -18,7 +18,15 @@ var speedX;
 var speedY;
 const BREEDTE = 20;
 
-
+var mensen = [  {x: 320,
+  y: 100,
+  speedX : 2,
+  speedY : -4},
+ {x: 500,
+  y: 490,
+  speedX : -3,
+  speedY : 1},
+];    
 
 /* ********************************************* */
 /* setup() en draw() functies / hoofdprogramma   */
@@ -33,11 +41,13 @@ function setup() {
   // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
   createCanvas(1280, 720);
   xPosities = [130, 60, 470];
-  yPosities = [700, 1010, 40];
+  yPosities = [50, 100, 40];
   // initialiseer waarden
   // midden van de hoogte van het canvas
-  speedX = random(-5, 5);      // random waarde tussen -5 en 5
-  speedY = random(-5, 5);      // 👆
+  speedX = [2, 5, 3];  // random waarde tussen -5 en 5
+  speedY = [4, 7, 6];  
+
+  var mensA = new Mens(300, 600, 2 -3);
 }
 
 /**
@@ -49,25 +59,42 @@ function draw() {
   // zwarte achtergrond
   background(0, 0, 0);
 
-  for (var i = 0; i < xPosities.length; i++) {
-    rect(xPosities[i], yPosities[i], BREEDTE, BREEDTE);
 
-  // teken
-  noStroke;
-  fill(255, 255, 255);
-  rect(xPosities[i], yPosities[i], BREEDTE, BREEDTE);
-
+  for (var i = 0; i < mensen.length; i++) {
+    rect(mensen[i].x, mensen[i].y, BREEDTE, BREEDTE);
+  
   // update positie
-  xPosities[i] = xPosities[i] + speedX;
-  yPosities[i] = yPosities[i] + speedY;
+  mensen[i].x = mensen[i].x + mensen[i].speedX;
+  mensen[i].y = mensen[i].y + mensen[i].speedY;
 
   // stuiter evt. tegen de kanten
-  if (xPosities[i] <= 0 || xPosities[i] + xPosities.length >= width) {
-    speedX = speedX * -1;
+  if (mensen[i].x <= 0 || mensen[i].x + BREEDTE >= width) {
+    mensen[i].speedX = mensen[i].speedX * -1;
   }
 
-  if (yPosities[i] <= 0 || yPosities[i] + yPosities.lentgh >= height) {
-    speedY = speedY * -1;
+  if (mensen[i].y <= 0 || mensen[i].y + BREEDTE >= height) {
+    mensen[i].speedY = mensen[i].speedY * -1;
   }
+  }
+}
+
+
+
+class Mens {
+  x;
+  y;
+  speedX;
+  speedY;
+
+  constructor(newX, newY, newSpeedX, newSpeedY) {
+    this.x = newX;
+    this.y = newY;
+    this.speedX = newSpeedX;
+    this.speedY = newSpeedY;
+  }
+
+  update() {
+    this.x = this.x - this.speedX;
+    this.y = this.y - this.speedY;
   }
 }
